1b6f31a05b87e670692cfd7509857b0529df3426
danie_codes <159900370+danieox@users.noreply.github.com>
2025-04-23 13:29:22 +0000

    Create problem6.py

---
+++ 1-10/problem6.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+
+# link to problem: https://projecteuler.net/problem=6
+
+a = 0
+b = 0
+
+for i in range (1,101):
+    a += i * i
+    b += i
+
+c = b*b
+d = c - a
+
+print("The sum of the squares of the first one hundred natural numbers is " + str(a))
+print("\n The square of the sum of the first one hundred natural numbers is " + str (b))
+print("\n Hence the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is " + str(d))
