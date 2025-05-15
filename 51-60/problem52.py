5fba68c0b29534bb22ce58f408fcebc9e98eae0e
danie_codes <159900370+danieox@users.noreply.github.com>
2025-05-15 17:14:33 +0000

    Create problem52.py

---
+++ 51-60/problem52.py
@@ -0,0 +1,16 @@
+for i in range(1,1000000):
+    a = str(i)
+    b = str(2*i)
+    c = str(3*i)
+    d = str(4*i)
+    e = str(5*i)
+    f = str(6*i)
+
+    new_set = {tuple(sorted(a))}
+
+    if (
+        len({len(a), len(b), len(c), len(d), len(e), len(f)}) == 1
+        and {tuple(sorted(b)), tuple(sorted(c)), tuple(sorted(d)), tuple(sorted(e)), tuple(sorted(f))} == new_set
+    ):
+        print(int(a))       
+
